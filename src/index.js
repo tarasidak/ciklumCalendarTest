9cd0558f9ba20862208af1de4bb2a434df0264e3
Taras Sidak <tarasidak1990@gmail.com>
2021-02-15 15:42:47 +0200

    add drag and drop

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ function addNewEvent() {
   const day = [...document.querySelectorAll('th')];
   const tbody = document.querySelector('tbody');
   const eventMemberSelected = document.querySelectorAll('.members option:checked');
+  const randomNumber = Math.floor(Math.random()*10000);
 
   for (let i = 0; i < day.length; i++) {
     if (eventDay.value === day[i].outerText) {
@@ -21,9 +22,10 @@ function addNewEvent() {
           if (tbody.rows[j].cells[i].childNodes.length === 0) {
             const values = Array.from(eventMemberSelected).map((el) => el.value);
             tbody.rows[j].cells[i].insertAdjacentHTML('afterbegin',
-              `<div class="personEvent ${values.join(' ')}"}>${eventName.value}<span data-bs-toggle="modal" data-bs-target="#exampleModalSec" class="deleteButton">X</span></div>`);
-            tbody.rows[j].cells[i].classList.add('insertEvent');
+              `<div class="personEvent ${values.join(' ')}" id="${randomNumber}" draggable="true"}>${eventName.value}
+                <span data-bs-toggle="modal" data-bs-target="#exampleModalSec" class="deleteButton">X</span></div>`);
             tbody.rows[j].cells[i].style.setProperty('background-color', 'lightgreen', 'important');
+            tbody.rows[j].cells[i].childNodes[0].addEventListener('dragstart', onDragStart);
           } else {
             alert('This slot is already busy!');
           }
@@ -40,7 +42,6 @@ root.onclick = function (event) {
   const deleteEvent = document.querySelector('.deleteEventButton');
   deleteEvent.addEventListener('click', () => {
     const personEvent = event.target.closest('.personEvent');
-    personEvent.parentElement.classList.remove('insertEvent');
     personEvent.parentElement.style.setProperty('background-color', 'white', 'important');
     personEvent.remove();
   });
@@ -71,3 +72,36 @@ function personalEvent() {
 
 chooseMember.addEventListener('change', personalEvent);
 createButton.addEventListener('click', addNewEvent);
+
+const tdElements = document.querySelectorAll('.td_item');
+
+function onDragStart(event) {
+  event
+    .dataTransfer
+    .setData('text/plain', event.target.id);
+    event.target.parentElement.style.setProperty('background-color', 'white', 'important');
+}
+
+function onDragOver(event) {
+  event.preventDefault();
+}
+
+function onDrop(event) {
+  const id = event
+    .dataTransfer
+    .getData('text');
+  const draggableElement = document.getElementById(id);
+  const dropzone = event.target;
+  if(dropzone.hasChildNodes()){
+    alert('This slot is busy');
+  } else {
+    dropzone.style.setProperty('background-color', 'lightgreen', 'important');
+    dropzone.appendChild(draggableElement);
+    event
+    .dataTransfer
+    .clearData();
+  }
+}
+
+tdElements.forEach(element => element.addEventListener('dragover', onDragOver));
+tdElements.forEach(element => element.addEventListener('drop', onDrop));
