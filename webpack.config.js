c730ebf409ea180fed7dc1fe24a57b2e9f26d295
Taras Sidak <tarasidak1990@gmail.com>
2021-02-15 17:18:10 +0200

    add drag and drop

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,13 @@ module.exports = {
     main: path.resolve(__dirname, './src/index.js'),
   },
   output: {
-    path: path.resolve(__dirname, '../docs'),
+    path: path.resolve(__dirname, './docs'),
     filename: '[name].bundle.js',
   },
   mode: 'development',
   devServer: {
     historyApiFallback: true,
-    contentBase: path.resolve(__dirname, '../docs'),
+    contentBase: path.resolve(__dirname, './docs'),
     open: true,
     compress: true,
     hot: true,
